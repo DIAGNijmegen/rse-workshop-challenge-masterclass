bdac94df7f0b273b38400595b6349a1c3ae517fa
amickan <amickan1990@gmail.com>
2024-10-14 16:13:03 +0200

    Add first version of vessel segmentation algorithm

--- algorithms/inference.py
+++ algorithms/inference.py
@@ -17,48 +17,97 @@ Any container that shows the same behavior will do, this is purely an example of
 
 Happy programming!
 """
-from pathlib import Path
+import SimpleITK
+import numpy as np
+import torch
+import monai
 import json
 from glob import glob
-import SimpleITK
-import random
+from pathlib import Path
+from scipy.special import expit
+from skimage import transform
+
 
 def run():
-    # Read the input
-    input_mammal = load_image_file_as_array(
-        location= Path("/input") / "images" / "mammal",
+    ## Read the inputs
+    # OCT image
+    input_oct_image = load_image_file_as_array(
+        location= Path("/input") / "images" / "oct",
     )
+    # dummy patient metadata that we will ignore
+    # this is just to demonstrate the possibility of having multiple different inputs
     input_age_in_months = load_json_file(
          location=Path("/input") / "age-in-months.json",
     )
-    
-    # For now, let us make bogus predictions
-    output_uncertainty_score = 0.5
-    output_cat_or_dog = random.choice(["feline", "canine"])
+
+    # preprocess image
+    original_shape = input_oct_image.shape
+    preprocessed_oct_image = preprocess_image(image=input_oct_image)
+
+    # load model and predict 
+    result = predict(
+        image=preprocessed_oct_image, 
+    )
+
+    # post-process image
+    output_vessel_segmentation = postprocess_image(image=result, original_shape=original_shape)
 
     # Save your output
-    write_json_file(
-        location=Path("/output")  / "uncertainty-score.json",
-        content=output_uncertainty_score
-    )
-    write_json_file(
-        location=Path("/output") / "cat_or_dog.json",
-        content=output_cat_or_dog
+    write_array_as_image_file(
+        location=Path("/output") / "images/vessel-segmentation",
+        array=output_vessel_segmentation,
     )
     
     return 0
+
+
+def preprocess_image(*, image):
+    """Resize, normalize, and transpose the input image."""
+    image = transform.resize(image, (512, 512), order=3) 
+    image = image.astype(np.float32) / 255.  # normalize
+    image = image.transpose((2, 0, 1))  # flip the axes and bring color to the first channel
+    return image 
+
+
+def postprocess_image(*, image, original_shape):
+    """Resize the predicted image, binarize, and convert to uint8."""
+    image = transform.resize(image, original_shape[:-1], order=3)
+    image = (expit(image) > 0.99)  # apply the sigmoid filter and binarize the predictions
+    image = (image * 255).astype(np.uint8) # cast to uint8, as expected for segmentations on Grand Challenge
+    return image 
+
+
+def predict(*, image):
+    """Predict using a pre-trained UNet model."""
+    device = torch.device('cpu')
+    image = torch.from_numpy(image).to(device).reshape(1, 3, 512, 512)
+    model = monai.networks.nets.UNet(
+        spatial_dims=2,
+        in_channels=3,
+        out_channels=1,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+    ).to(device)
+
+    model.eval()
+
+    model.load_state_dict(
+        torch.load(
+            "model/model.pth",
+            map_location=device,
+        )
+    )
+
+    result = model(image).squeeze().data.cpu().numpy()
+
+    return result
 
 
 def load_json_file(*, location):
     # Reads a json file
     with open(location, 'r') as f:
         return json.loads(f.read())
-
-
-def write_json_file(*, location, content):
-    # Writes a json file
-    with open(location, 'w') as f:
-        f.write(json.dumps(content, indent=4))
 
 
 def load_image_file_as_array(*, location):
@@ -68,6 +117,19 @@ def load_image_file_as_array(*, location):
 
     # Convert it to a Numpy array
     return SimpleITK.GetArrayFromImage(result)
+
+
+def write_array_as_image_file(*, location, array):
+    location.mkdir(parents=True, exist_ok=True)
+    suffix = ".mha"
+
+    image = SimpleITK.GetImageFromArray(array)
+    SimpleITK.WriteImage(
+        image,
+        location / f"output{suffix}",
+        useCompression=True,
+    )
+
 
 if __name__ == "__main__":
     raise SystemExit(run())
