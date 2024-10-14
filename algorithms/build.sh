3aea85ea4330efdd37de06b595bdd5dde27ec644
Chris van Run <Chris.vanrun@radboudumc.nl>
2024-10-14 13:58:30 +0200

    Add build-arg, to ensure unique image at every build

--- algorithms/build.sh
+++ algorithms/build.sh
@@ -12,6 +12,8 @@ if [ "$#" -eq 1 ]; then
     DOCKER_TAG="$1"
 fi
 
+# Note: the build-arg is JUST for the workshop
 docker build "$SCRIPT_DIR" \
   --platform=linux/amd64 \
+  --build-arg BUILD_VERSION=$(date +%s) \
   --tag $DOCKER_TAG 2>&1
